a4f35b24c345de4ef30f32facd5de4374748f3ba
Regis Freyd <djaiss@users.noreply.github.com>
2023-11-12 13:40:24 -0500

    feat: add language file system and locale middleware

---
+++ resources/views/components/link.blade.php
@@ -0,0 +1,3 @@
+<a {{ $attributes->merge(['class' => 'text-blue-700 underline hover:bg-blue-700 hover:text-white transition duration-200']) }}>
+  {{ $slot }}
+</a>
--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,14 @@
 <?php
 
+use App\Http\Controllers\LocaleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('locale/{locale}', [LocaleController::class, 'update'])->name('locale.update');
 
 Route::get('dashboard', function () {
     return view('dashboard');
