8890cae56b6251898042324df019d20beff928ec
Regis Freyd <djaiss@users.noreply.github.com>
2023-11-12 21:38:25 -0500

    Refactor link and locale switch components

--- resources/views/components/link.blade.php
+++ resources/views/components/link.blade.php
@@ -1,4 +1,6 @@
-<a hx-boost="true"
+@props(['boost' => 'true'])
+
+<a hx-boost="{{ $boost }}"
    {{ $attributes->merge(['class' => 'text-blue-700 underline hover:no-underline dark:text-gray-400 dark:hover:text-gray-100 dark:focus:ring-offset-gray-800']) }}>
   {{ $slot }}
 </a>
