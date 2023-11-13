68b2c26635889426312abae79926faf952bea2f7
Regis Freyd <djaiss@users.noreply.github.com>
2023-11-13 18:30:14 -0500

    feat: main ui

--- routes/web.php
+++ routes/web.php
@@ -14,10 +14,12 @@ Route::get('dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function (): void {
+Route::middleware(['auth', 'verified'])->group(function (): void {
     Route::get('profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('settings', [SettingsController::class, 'index'])->name('settings.index');
 });
 
 require __DIR__ . '/auth.php';
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+        sans: [...defaultTheme.fontFamily.sans],
       },
     },
   },
