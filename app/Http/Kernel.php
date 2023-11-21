6371bbdf1edc80f305618033971c9f4d70d5d8b2
Mazarin <djaiss@users.noreply.github.com>
2023-11-21 07:23:38 -0500

    feat: list users (#26)

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -66,5 +66,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'administrator' => \App\Http\Middleware\CheckAdministratorRole::class,
+        'user' => \App\Http\Middleware\CheckUser::class,
     ];
 }
